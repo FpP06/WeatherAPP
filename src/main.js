62ecdf9a54049eba896ed9a77eaab9d930de5f80
FpP06 <pisniak21.filip@gmail.com>
2023-05-27 15:47:57 +0200

    Added function displayWeatherData

--- src/main.js
+++ src/main.js
@@ -30,15 +30,48 @@ const setupListeners = () => {
 }
 
 const onClickSubmit = (event) => {
+    getWeatherByCity(viewElements.searchInput.value).then(data => {
+        displayWeatherData(data);
+    });
 }
 
 const onEnterSubmit = (event) => {
     if(event.key === 'Enter') {
         getWeatherByCity(viewElements.searchInput.value).then(data => {
-            console.log("OK")
-            console.log(data.resolvedAddress);
-            console.log(data.currentConditions.temp);
+            displayWeatherData(data);
         });
+    }
+}
+
+const displayWeatherData = (data) => {
+    switchView();
+    // fadeInOut();
+    console.log("OK")
+    console.log(data.resolvedAddress);
+    console.log(data.currentConditions.temp);
+    viewElements.weatherCity.textContent = data.resolvedAddress;
+    viewElements.weatherCurrentTemp.textContent = data.currentConditions.temp;
+    viewElements.weatherMaxTemp.textContent = data.days[0].tempmax;
+    viewElements.weatherMinTemp.textContent = data.days[0].tempmin;
+}
+
+const switchView = () => {
+    if(viewElements.weatherSearchView.style.display !== 'none') {
+        viewElements.weatherSearchView.style.display = 'none';
+        viewElements.weatherForecastView.style.display = 'block';
+    }
+    else {
+        viewElements.weatherSearchView.style.display = 'block';
+        viewElements.weatherForecastView.style.display = 'none';
+    }
+}
+
+const fadeInOut = () => {
+    if (viewElements.mainContainer.style.opacity === '1' || viewElements.mainContainer.style.opacity === '') {
+        viewElements.mainContainer.style.opacity = '0';
+    } 
+    else {
+        viewElements.mainContainer.style.opacity = '1';
     }
 }
 
